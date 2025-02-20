2ac6f24c8d97cf977b1cc2f7506635c249b6ed6b
tontonkurakura <tm.38k05519whm@gmail.com>
2025-02-21 00:41:24 +0900

    tidy up done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30日間のキャッシュ
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // クライアントサイドのビルドでは、sharp関連のモジュールを空のモジュールに置き換える
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        sharp: false,
+        "detect-libc": false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
